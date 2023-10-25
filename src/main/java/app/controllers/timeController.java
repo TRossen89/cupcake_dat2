c702bb024c8c85661f1279ca5755194e8fcd0ddf
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-10-25 14:36:10 +0200

    Calendar method

--- src/main/java/app/controllers/timeController.java
+++ src/main/java/app/controllers/timeController.java
@@ -2,13 +2,16 @@ package app.controllers;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.Date;
 
 public class timeController {
     public timeController() {
 
-        Date date = new Date(System.currentTimeMillis());
-        System.out.println(date);
+        Calendar currentTime = Calendar.getInstance();
+
+        System.out.println(currentTime.get(Calendar.HOUR_OF_DAY) +":" + currentTime.get(Calendar.MINUTE));
     }
 }
