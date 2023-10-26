abb54e8f92666c53d3a7d05848b049a928e0ee5e
DavidAH <cph-dh223@cphbusiness.dk>
2023-10-26 13:38:17 +0200

    Admin backend

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
+import java.sql.SQLException;
+
 import app.config.ThymeleafConfig;
+import app.controllers.AdminControler;
 import app.controllers.UserControler;
+import app.persistence.AdminMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
@@ -16,6 +20,7 @@ public class Main {
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
     public static void main(String[] args)
     {
+
         // Initializing Javalin and Jetty webserver
 
         Javalin app = Javalin.create(config -> {
@@ -30,6 +35,7 @@ public class Main {
         app.get("/createUser", ctx -> ctx.render("createUser.html"));
         app.post("/createUser", ctx -> UserControler.createUser(ctx, connectionPool));
       
-        app.get("/adminpage", ctx -> ctx.render("adminpage.html"));
+        app.get("/adminpage", ctx -> AdminControler.renderAdminPage(ctx, connectionPool));
+        app.post("/adminOrderLine", ctx -> AdminControler.getOrderLine(ctx, connectionPool));
     }
 }
