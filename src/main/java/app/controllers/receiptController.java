58a9a646e2ad6626b0a8da2e6206c7ab5043591c
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-10-27 11:26:46 +0200

    Fixing some issues

--- src/main/java/app/controllers/receiptController.java
+++ src/main/java/app/controllers/receiptController.java
@@ -7,7 +7,7 @@ public class receiptController {
 
         Calendar currentTime = Calendar.getInstance();
 
-        String timeReady = (Calendar.HOUR_OF_DAY) +":" + currentTime.get(Calendar.MINUTE);
+        String timeReady = currentTime.get(Calendar.HOUR_OF_DAY) +":" + currentTime.get(Calendar.MINUTE);
         String hours = String.valueOf(currentTime.get(Calendar.HOUR_OF_DAY));
         String minutes = String.valueOf(currentTime.get(Calendar.MINUTE));
 
