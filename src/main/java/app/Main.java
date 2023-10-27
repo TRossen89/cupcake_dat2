99ed849d795209fbc26ef04c478c4e9e1c4e7187
cph-dh223 <124783509+cph-dh223@users.noreply.github.com>
2023-10-27 11:15:00 +0200

    Admin backend (#44)

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,14 @@
 package app;
 
+import java.sql.SQLException;
+
 import app.config.ThymeleafConfig;
-import app.controllers.UserController;
+
+import app.controllers.AdminControler;
+import app.controllers.UserControler;
+import app.persistence.AdminMapper;
 import app.entities.User;
+
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
@@ -18,6 +24,7 @@ public class Main {
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
     public static void main(String[] args)
     {
+
         // Initializing Javalin and Jetty webserver
 
         Javalin app = Javalin.create(config -> {
@@ -32,7 +39,8 @@ public class Main {
         app.get("/createUser", ctx -> ctx.render("createUser.html"));
         app.post("/createUser", ctx -> UserController.createUser(ctx, connectionPool));
       
-        app.get("/adminpage", ctx -> ctx.render("adminpage.html"));
+        app.get("/adminpage", ctx -> AdminControler.renderAdminPage(ctx, connectionPool));
+        app.post("/adminOrderLine", ctx -> AdminControler.getOrderLine(ctx, connectionPool));
     }
 
 
--- src/main/java/app/entities/Orderline.java
+++ src/main/java/app/entities/Orderline.java
@@ -2,15 +2,24 @@ package app.entities;
 
 public class Orderline {
 
-    String bottom;
-    String topping;
-    int quantity;
-    double totalPrice;
+    public int id;
+    public String bottom;
+    public String topping;
+    public int quantity;
+    public double totalPrice;
 
-    public Orderline(String bottom, String topping, int quantity, double totalPrice) {
+    public Orderline(int id, String bottom, String topping, int quantity, double totalPrice) {
+        this.id = id;
         this.bottom = bottom;
         this.topping = topping;
         this.quantity = quantity;
         this.totalPrice = totalPrice;
     }
+
+    @Override
+    public String toString() {
+        return "Orderline [id=" + id + ", bottom=" + bottom + ", topping=" + topping + ", quantity=" + quantity
+                + ", totalPrice=" + totalPrice + "]";
+    }
+    
 }
